ea7e66fddb87dc378d6afd15328c60bd0d7ec108
Luis Lopez <luisangellopz.snchz999@gmail.com>
2022-03-21 14:51:10 -0600

    make it better

--- src/db/config.ts
+++ src/db/config.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const dbConnection = async () => {
   try {
     await mongoose.connect(process.env.MDB_CNN || "");
+
     console.log("DB CONNECT");
   } catch (error) {
     console.log(error);
