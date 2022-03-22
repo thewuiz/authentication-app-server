3055ca6bb020e114fafdb5839d0b525008e2dba3
Luis Lopez <luisangellopz.snchz999@gmail.com>
2022-03-21 18:40:45 -0600

    make it better

--- src/db/config.ts
+++ src/db/config.ts
@@ -6,6 +6,7 @@ const dbConnection = async () => {
     console.log(mongo_url);
     await mongoose.connect(mongo_url);
   } catch (error) {
+    console.log("DB CONNECT ERROR");
     const err = new Error(`DB CONNECT ERROR: ${error} `);
     return err;
   }
